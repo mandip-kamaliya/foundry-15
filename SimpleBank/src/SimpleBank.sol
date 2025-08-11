e2fdea1c15c24e30fc369c09a926c76661a2d7d8
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-08-11 11:25:17 +0530

    add SimpleBank Contract

---
+++ SimpleBank/src/SimpleBank.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+contract SimpleBank{
+
+    //mapings
+    mapping (address => uint) balances;
+
+    //events
+    event deposited(uint256 _amount , address _sender);
+    event Withdrawal(uint256 _amount , address _sender );
+
+
+    //functions
+    function deposit() public payable{
+        require(msg.value>0,"amount should be more than zero");
+       balances[msg.sender] += msg.value ;
+        emit deposited(msg.value , msg.sender);
+    }
+
+    function withdraw(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount , "user dont have enough funds to withdrw!!");
+        balances[msg.sender] -= _amount ;
+        emit Withdrawal(_amount , msg.sender);
+        (bool success , ) = msg.sender.call{value : _amount}("");
+        require(success,"withdrawal failed");
+    }
+}
