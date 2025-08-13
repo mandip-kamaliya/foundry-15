4a44663158f3958d8c27411dc23b4d0cd90ccd1a
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-08-13 09:38:45 +0530

    added setUp function

--- TokenSwap/test/TokenSwap.t.sol
+++ TokenSwap/test/TokenSwap.t.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/ERC20/ERC20.sol";
 
 contract mokeERC20 is ERC20{
 
-    constructor(string memory _name , string memory _symbol) ERC20(name,symbol) ;
+    constructor(string memory _name , string memory _symbol) ERC20(name,symbol){}
     function mint(address _to , uint256 _amount) public {
         _mint(_to , _amount);
     }
@@ -15,10 +15,13 @@ contract TokenSwapTest is Test{
     mokeERC20 public tokenA;
     mokeERC20 public tokenB;
     TokenSwap public smartcontact ;
+    address public owner = makeAddr("owner");
+    address public user = makeAddr("user");
 
     function setUp() public{
-       tokenA = new mokeERC20();
-       tokenB = new mokeERC20();
-       smartcontact = new TokenSwap();
+       tokenA = new mokeERC20("TOKEN A","TKA");
+       tokenB = new mokeERC20("TOKEN B","TKB");
+       vm.prank(owner);
+       smartcontact = new TokenSwap(address(tokenA) , address(tokenB));
     }
 }
