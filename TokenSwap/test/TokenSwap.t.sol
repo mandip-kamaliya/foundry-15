edc469dcb164b6ab9a16f61dd9a9de9c37ea1100
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-08-13 09:33:42 +0530

    added mokeERC20 contract

--- TokenSwap/test/TokenSwap.t.sol
+++ TokenSwap/test/TokenSwap.t.sol
@@ -1,7 +1,24 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 import "forge-std/Test.sol";
+import {TokenSwap} from "../src/TokenSwap.sol";
+import "@openzeppelin/contracts/ERC20/ERC20.sol";
 
+contract mokeERC20 is ERC20{
+
+    constructor(string memory _name , string memory _symbol) ERC20(name,symbol) ;
+    function mint(address _to , uint256 _amount) public {
+        _mint(_to , _amount);
+    }
+}
 contract TokenSwapTest is Test{
+    mokeERC20 public tokenA;
+    mokeERC20 public tokenB;
+    TokenSwap public smartcontact ;
 
+    function setUp() public{
+       tokenA = new mokeERC20();
+       tokenB = new mokeERC20();
+       smartcontact = new TokenSwap();
+    }
 }
