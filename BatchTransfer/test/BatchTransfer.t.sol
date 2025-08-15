77fb21a0850b1e610a973570cde5d0c51c8af7f6
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-08-15 15:17:31 +0530

    add setUp function

--- BatchTransfer/test/BatchTransfer.t.sol
+++ BatchTransfer/test/BatchTransfer.t.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-contract BatchTransferTest {
-    
+import "forge-std/Test.sol";
+import {BatchTransfer} from "../src/BatchTransfer.sol";
+contract BatchTransferTest is Test{
+    BatchTransfer public batchtransfer ; 
+
+    function setUp() public{
+        batchtransfer = new BatchTransfer();
+    }
+
+    function test_disburse() public {
+        
+    }
+
 }
