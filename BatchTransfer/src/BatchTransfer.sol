772dddf16b021e476bfe7ff6374c39c17e3b52c6
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-08-15 15:10:16 +0530

    added BatchTransfer.sol file

---
+++ BatchTransfer/src/BatchTransfer.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+contract BatchTransfer{
+    function disburse(address payable[] calldata _recipients) public payable{
+        uint256 recipientlength = _recipients.length;
+        require(recipientlength > 0 , "recipients are empty");
+        require(msg.value > 0 , "amount should be more than zero");
+
+        uint256 amountperrecipient = msg.value / recipientlength ;
+
+        for(uint256 i = 0 ; i < recipientlength ; i++){
+           _recipients[i].transfer(amountperrecipient);
+        }
+    }
+}
