26c1b7c607c38054f96471f659600a072ee87360
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-08-16 22:43:04 +0530

    added test_bid function

--- Auction/test/Auction.t.sol
+++ Auction/test/Auction.t.sol
@@ -5,8 +5,16 @@ import "forge-std/Test.sol";
 import {Auction} from "../src/Auction.sol";
 contract AuctionTest is Test{
     Auction public auction;
+    address user = makeAddr("user");
 
     function setUp() public{
-        auction = new Auction();
+        auction = new Auction(200000);
+        vm.deal(user , 5 ether);
+    }
+
+    function test_bid() public{
+        vm.prank(user);
+        auction.bid{value : 1 ether }();
+        assertEq(address(auction).balance , 1 ether);
     }
 }
