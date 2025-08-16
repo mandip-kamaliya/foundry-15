2bfbcaa40d052ace562fa207bc713ca326c6d5ce
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-08-16 22:03:03 +0530

    added constructor

--- Auction/src/Auction.sol
+++ Auction/src/Auction.sol
@@ -5,7 +5,13 @@ contract Auction {
     uint256 public highestbid;
     address public highestbidder;
     uint256 public auctionendtime;
+    address owner;
     bool ended;
+
+    constructor(uint256 duration){
+        owner = msg.sender;
+        auctionendtime = block.timestamp + duration ;
+    }
 
     function bid() public payable{
         require(msg.value > highestbid , "bid is not acceptable");
