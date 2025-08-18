d7864fd8b09f67ad8fe81b4922f54f8c27a06e32
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-08-18 18:15:19 +0530

    added events in voting contract

--- voting/src/voting.sol
+++ voting/src/voting.sol
@@ -3,9 +3,16 @@ pragma solidity ^0.8.20;
 
 contract voting {
     string memory proposol ;
+    uint256 immutable public fees = 0.01 ether ;
     mapping (uint256 => string ) public indexTOproposol;
+    mapping (uint256 => proposol) public votesOnproposol;
 
-    function vote() public{
+    event voted(address indexed _sender );
+
+    function vote(uint256 _fees) public payable{
+        (bool sent,) = address(this).call{value:_fees}();
+        require("sent","transation failed");
         
+        emit voted(msg.sender);
     }
 }
