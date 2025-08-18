11ab0c6ae32929ac18c7505ac9ee6928cee141fa
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-08-18 11:42:18 +0530

    added state variables

---
+++ voting/src/voting.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+contract voting {
+    string memory proposol ;
+    mapping (uint256 => string ) public indexTOproposol;
+
+    function vote() public{
+        
+    }
+}
