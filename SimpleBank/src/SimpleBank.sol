1b6ff7558b3e8787750ebc292d1e6c6ad5396315
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-08-22 11:29:52 +0530

    fix:workflow by correct formating

--- SimpleBank/src/SimpleBank.sol
+++ SimpleBank/src/SimpleBank.sol
@@ -1,32 +1,30 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-contract SimpleBank{
-
+contract SimpleBank {
     //mapings
-    mapping (address => uint) public balances;
+    mapping(address => uint256) public balances;
 
     //events
-    event deposited(uint256 _amount , address _sender);
-    event Withdrawal(uint256 _amount , address _sender );
-
+    event deposited(uint256 _amount, address _sender);
+    event Withdrawal(uint256 _amount, address _sender);
 
     //functions
-    function deposit() public payable{
-        require(msg.value>0,"amount should be more than zero");
-        balances[msg.sender] += msg.value ;
-        emit deposited(msg.value , msg.sender);
+    function deposit() public payable {
+        require(msg.value > 0, "amount should be more than zero");
+        balances[msg.sender] += msg.value;
+        emit deposited(msg.value, msg.sender);
     }
 
-      function withdraw(uint256 _amount) public {
-        require(balances[msg.sender] >= _amount , "user dont have enough funds to withdraw!!");
-        balances[msg.sender] -= _amount ;
-        emit Withdrawal(_amount , msg.sender);
-        (bool success , ) = msg.sender.call{value : _amount}("");
-        require(success,"withdrawal failed");
+    function withdraw(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "user dont have enough funds to withdraw!!");
+        balances[msg.sender] -= _amount;
+        emit Withdrawal(_amount, msg.sender);
+        (bool success,) = msg.sender.call{value: _amount}("");
+        require(success, "withdrawal failed");
     }
 
-    function getBalance(address _sender) public view returns(uint256){
+    function getBalance(address _sender) public view returns (uint256) {
         return balances[_sender];
     }
 }
--- SimpleBank/test/SimpleBank.t.sol
+++ SimpleBank/test/SimpleBank.t.sol
@@ -4,57 +4,56 @@ pragma solidity ^0.8.20;
 import {Test} from "forge-std/Test.sol";
 import {SimpleBank} from "../src/SimpleBank.sol";
 
-
-
-contract SimpleBankTest is Test{
+contract SimpleBankTest is Test {
     SimpleBank simplebank;
     address user = makeAddr("user");
     address adam = makeAddr("adam");
-     function setUp() public {
+
+    function setUp() public {
         simplebank = new SimpleBank();
-     }
+    }
 
-     function test_deposit() public{
+    function test_deposit() public {
         vm.startPrank(user);
-        vm.deal(user,1 ether);
-        simplebank.deposit{value : 0.01 ether}();
-        assertEq(simplebank.getBalance(user),0.01 ether);
+        vm.deal(user, 1 ether);
+        simplebank.deposit{value: 0.01 ether}();
+        assertEq(simplebank.getBalance(user), 0.01 ether);
         vm.stopPrank();
-     }
+    }
 
-     function test_withdraw() public{
+    function test_withdraw() public {
         vm.startPrank(user);
-        vm.deal(user,1 ether);
-        simplebank.deposit{value : 0.01 ether}();
+        vm.deal(user, 1 ether);
+        simplebank.deposit{value: 0.01 ether}();
         simplebank.withdraw(0.01 ether);
-        assertEq(simplebank.getBalance(user),0);
+        assertEq(simplebank.getBalance(user), 0);
         vm.stopPrank();
-     }
+    }
 
-     function test_amountcheck() public{
+    function test_amountcheck() public {
         vm.startPrank(user);
-        vm.deal(user,1 ether);
-        simplebank.deposit{value:0.01 ether}();
-        assertEq(simplebank.balances(user),0.01 ether);
+        vm.deal(user, 1 ether);
+        simplebank.deposit{value: 0.01 ether}();
+        assertEq(simplebank.balances(user), 0.01 ether);
         vm.stopPrank();
-     }
+    }
 
-     function test_revert_depositzero() public{
+    function test_revert_depositzero() public {
         vm.expectRevert("amount should be more than zero");
         vm.prank(user);
-        simplebank.deposit{value:0 }();
-     }
+        simplebank.deposit{value: 0}();
+    }
 
-     function test_revert_withdrawWithoutDeposit() public{
+    function test_revert_withdrawWithoutDeposit() public {
         vm.expectRevert("user dont have enough funds to withdraw!!");
         vm.prank(user);
         simplebank.withdraw(0.01 ether);
-     }
+    }
 
-     function test_revert_withdrawMoreThanBalance() public {
+    function test_revert_withdrawMoreThanBalance() public {
         // Arrange: User deposits 1 ETH
         vm.prank(user);
-        vm.deal(user,1 ether);
+        vm.deal(user, 1 ether);
         simplebank.deposit{value: 1 ether}();
 
         // Act & Assert: User tries to withdraw 2 ETH
@@ -63,33 +62,28 @@ contract SimpleBankTest is Test{
         simplebank.withdraw(2 ether);
     }
 
-     function test_withdrawZeroAmount() public {
-    
+    function test_withdrawZeroAmount() public {
         vm.prank(user);
-        vm.deal(user,1 ether);
+        vm.deal(user, 1 ether);
         simplebank.deposit{value: 1 ether}();
         assertEq(simplebank.getBalance(user), 1 ether);
 
-     
         vm.prank(user);
         simplebank.withdraw(0);
 
-       
         assertEq(simplebank.getBalance(user), 1 ether);
     }
 
-     function test_withdrawExactBalance() public {
+    function test_withdrawExactBalance() public {
         // Arrange
         vm.prank(user);
-        vm.deal(user,5 ether);
+        vm.deal(user, 5 ether);
         simplebank.deposit{value: 5 ether}();
         assertEq(simplebank.getBalance(user), 5 ether);
 
-        
         vm.prank(user);
         simplebank.withdraw(5 ether);
 
-     
         assertEq(simplebank.getBalance(user), 0);
     }
 }
