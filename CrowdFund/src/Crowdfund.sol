f2c9ce0c306e6fcf73d00a891bdd4b579c3cea3f
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-08-24 16:59:24 +0530

    add fundCampaign function

--- CrowdFund/src/Crowdfund.sol
+++ CrowdFund/src/Crowdfund.sol
@@ -3,7 +3,23 @@ pragma solidity ^0.8.20;
 
 contract crowdfund{
 
+    struct Campaign{
+        string name;
+        uint256 id;
+    }
+    Campaign[] public campaigns;
+    mapping (address=>Campaign) public userTocampaign;
+    mapping (address=>uint256) public userfunded;
+
+    event Funded(address indexed _sender,uint256 _amount);
+
+    function fundCampaign(uint256 _id) public payable{
+        require(msg.value > 0 , "value should be more than zero!!");
+        userfunded[msg.sender]+=msg.value;
+        emit Funded(msg.sender,msg.value);
+    }
     
+
 
 
 }
