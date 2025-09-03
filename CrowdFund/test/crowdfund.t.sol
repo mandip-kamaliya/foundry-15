bea5101b8e5e60d1b666a7eb3f459553ec3c1779
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-03 19:12:23 +0530

    added test_fundCampaign

--- CrowdFund/test/crowdfund.t.sol
+++ CrowdFund/test/crowdfund.t.sol
@@ -4,6 +4,7 @@ import {Test} from "forge-std/Test.sol";
 import {crowdfund} from "../src/Crowdfund.sol";
 contract crowdfundTest is Test{
     crowdfund public Crowdfund;
+    address user = makeAddr("user");
 
     function setUp() public{
         Crowdfund = new crowdfund();
@@ -15,5 +16,14 @@ contract crowdfundTest is Test{
         assertEq(owner, address(this));
         assertEq(name, "Test Campaign");
         assertEq(campaignId, id);
+    }
+
+    function test_fundCampaign() public{
+        uint256 id = Crowdfund.addCampaign("Test Campaign");
+        (address owner, string memory name, uint256 campaignId) = Crowdfund.campaigns(0);
+        vm.deal(user,1 ether);
+        vm.prank(user);
+        Crowdfund.fundCampaign{value:1 ether}(id);
+        assertEq(Crowdfund.contributions(id,user),1 ether);
     }
 }
