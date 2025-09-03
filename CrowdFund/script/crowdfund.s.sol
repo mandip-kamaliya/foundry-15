8b7ef2120d0aea88f1444406d9d2f3751d827fb9
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-09-03 18:47:27 +0530

    added script file

---
+++ CrowdFund/script/crowdfund.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {crowdfund} from "../src/Crowdfund.sol";
+contract CrowdfundScript is Script{
+    crowdfund public crowdFund;
+
+    function setUp() public{
+        crowdFund = new crowdfund();
+    }
+}
